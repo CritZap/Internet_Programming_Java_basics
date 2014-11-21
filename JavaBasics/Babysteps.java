9e214f4838b58410b3c6162521bf4e8c627ad34e
Alexander Monov <alekmonov@abv.bg>
2014-11-21 21:28:17 +0200

    Create Babysteps.java

---
+++ JavaBasics/Babysteps.java
@@ -0,0 +1,20 @@
+
+public class Babysteps {
+	public static void main(String args[]) {
+		int a = 5; // Def a as an integer and give it a value
+		String b; // Def b as a string
+		b = "Boo"; // Give it a value
+		
+		System.out.println("a: " + a + "  b: " + b); 
+		// Shows on the console
+		
+		for (int i=1;i<=10;i++) {
+			System.out.println(i);
+			if (i%2 != 0) {
+				System.out.println(i);
+			}
+		}
+		//To show the odd ones only just comment the first print line
+		
+	}
+}
